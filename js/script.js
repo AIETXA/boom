4659b633a35e0f0ba5a1f50f97b242a2c2174ab4
Ailén <ailenechaguibel@gmail.com>
2025-04-01 14:11:25 +0200

    trabajo no finalizado pero no logro hacer lo que me falta (cuenta atras y mensajes)

--- js/script.js
+++ js/script.js
@@ -8,35 +8,109 @@
 */
 
 
-const restart = document.getElementById('restart');
-const countdown = document.getElementById('countdown');
-const result = document.getElementById('result');
+
+const compuCountdown = document.getElementById('countdown');
 const userInput = document.getElementById('userInput');
+const resultado = document.getElementById('result');
+const cuentaRegresiva = document.getElementById('cuentaRegresiva');
+const resultadoFinal = document.getElementById('resultadoFinal');
+const empezarDeNuevo = document.getElementById('restart');
 let numerosAleatorios 
-
-restart.addEventListener('click', () => {
-
-}) //boton reiniciar ok
-  
-
 const numeroDelUsuario = prompt ('Elige un número del 1 al 3');
+
+
 
 
 function introducirNumero() {
    if (numeroDelUsuario) {
         userInput.value = numeroDelUsuario}
-        console.log(numeroDelUsuario);
+        
 };
 introducirNumero()
 
+
 const contador = new Promise((resolve) => {
-    setTimeout(() => {
-        numerosAleatorios = Math.floor(Math.random()*3) + 1;
-        resolve(numerosAleatorios);
-    },5000)
+ setTimeout(() => {
+     numerosAleatorios = Math.floor(Math.random()*3) + 1;
+     resolve(numerosAleatorios);
+ },5000)
 });
 contador.then((numerosAleatorios) =>
-    countdown.innerText = numerosAleatorios
+compuCountdown.innerText = numerosAleatorios
 );
 
-contador() 
+//contador();
+
+
+//hasta aca funciona bien
+
+
+
+//no funciona y no se porque
+
+function resultadoDelJuego() {
+  compuCountdown.textContent = numerosAleatorios;
+  userInput.textContent = numeroDelUsuario;
+  resultadoFinal.textContent = userInput && compuCountdown;
+
+if(numeroDelUsuario === numerosAleatorios) {
+  resultadoFinal.innerHTML = '¡Has salvado el mundo!';
+} else {
+  resultadoFinal.innerHTML ='La bomba ha estallado';
+}
+
+};
+
+
+// Función de cuenta atrás utilizando async/await
+async function startCountdown() {
+  for (let i = cuentaRegresiva; i > 0; i--) {
+    cuentaRegresiva.textContent = i;
+      await boom(1000);  // Espera 1 segundo antes de continuar
+  }
+}
+
+ 
+
+
+
+
+function resetGame() {
+  userInput = parseInt(prompt("Introduce un número del 1 al 3:")) || 0;
+  compuCountdown = Math.floor(Math.random() * 3) + 1;
+  userInput.textContent = 0;
+  compuCountdown.textContent = 0;
+ 
+  
+}
+
+empezarDeNuevo.addEventListener("click", resetGame);
+
+
+
+
+
+
+
+
+
+
+
+  
+//resultado del juego
+
+
+       
+//boton reiniciar ok
+ /* 
+
+
+        
+};
+
+*/
+//funcion resetear ok
+/*
+
+});
+*/
